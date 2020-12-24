10c1d9fdd6c09d0d22fd41d33030ccd644003e18
17521236 <17521236@gm.uit.edu.vn>
2020-12-25 03:29:10 +0700

    about us page, contact page

--- js/home.js
+++ js/home.js
@@ -1,4 +1,3 @@
-
 var arr = document.querySelectorAll('.question__answer');
 var preId = 5;
 
@@ -6,4 +5,24 @@ var show = (id) => {
     arr[id].classList.add('question__answer--show');
     arr[preId].classList.remove('question__answer--show');
     preId = id;
+}
+
+var navMobile = document.getElementById('navbar-mobile');
+var btnHide = document.getElementById('btn-hide');
+var btnShow = document.getElementById('btn-show');
+
+var isShowBtnMenu = true;
+
+var showNav = () => {
+
+    if (isShowBtnMenu) {
+        btnShow.style.display = 'none';
+        btnHide.style.display = 'block';
+        navMobile.classList.add('navbar-mobile--show');
+    } else {
+        btnShow.style.display = 'block';
+        btnHide.style.display = 'none';
+        navMobile.classList.remove('navbar-mobile--show');
+    }
+    isShowBtnMenu = !isShowBtnMenu;
 }
